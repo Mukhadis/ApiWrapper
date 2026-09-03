0634d88477acf3db117ea88b18c94ceef63f086a
Mukhadis Yaru <mukyar@amazon.com>
2026-09-03 23:10:56 +0100

    chore: Added error handling for API issues

--- app/constants.py
+++ app/constants.py
@@ -1,6 +1,7 @@
 city = "Dublin"
 
 coord_url = f"https://geocoding-api.open-meteo.com/v1/search"
+break_test_url = f"https://nonesense-url.ie"
 weather_url = f"https://api.open-meteo.com/v1/forecast"
 
 location_params = {
--- app/main.py
+++ app/main.py
@@ -1,34 +1,43 @@
-from .constants import city, coord_url, weather_url, location_params
+from __future__ import annotations
+
+from .constants import city, coord_url, weather_url, location_params, break_test_url
+from typing import Any
 
 import requests
 import time
 
+def get_latitude_and_longitude() -> tuple[int, int] | str:
+    try:
+        response = requests.get(coord_url, params=location_params)
+        data = response.json()
+        results = data["results"]
+        relevant_info = results[0]
+        latitude = relevant_info["latitude"]
+        longitude = relevant_info["longitude"]
+        coordinates = (latitude, longitude)
+        return coordinates
+    except requests.exceptions.ConnectionError:
+        return "Error: Could not reach the geocoding API"
 
-def get_latitude_and_longitude() -> tuple[str, str]:
-    response = requests.get(coord_url, params=location_params)
-    data = response.json()
-    results = data["results"]
-    relevant_info = results[0]
-    latitude = relevant_info["latitude"]
-    longitude = relevant_info["longitude"]
-    coordinates = (latitude, longitude)
-    return coordinates
-
-def get_weather() -> str:
-    coords = get_latitude_and_longitude()
+def get_weather(coordinates: tuple[int, int] | str) -> tuple[int, int] | str:
     weather_params = {
-        "latitude": coords[0],
-        "longitude": coords[1],
+        "latitude": coordinates[0],
+        "longitude": coordinates[1],
         "hourly": "temperature_2m"
     }
-    response = requests.get(weather_url, params=weather_params)
-    data = response.json()
-    hourly_weather = data["hourly"]
-    current_hour = time.localtime().tm_hour
-    weather_that_hour = hourly_weather["temperature_2m"]
-    return f"Weather is currently {weather_that_hour[current_hour]}º celsius in {city}."
+    try:
+        response = requests.get(weather_url, params=weather_params)
+        data = response.json()
+        hourly_weather = data["hourly"]
+        current_hour = time.localtime().tm_hour
+        weather_that_hour = hourly_weather["temperature_2m"]
+        return f"Weather is currently {weather_that_hour[current_hour]}º celsius in {city}."
+    except requests.exceptions.ConnectionError:
+        return f"Could not reach the weather API"
+    except KeyError:
+        return coordinates
 
 def main():
-    print(get_weather())
+    print(get_weather(get_latitude_and_longitude()))
 
 main()
