94cb010bf5a217c28d880f7101cb11eb8f280e29
Mukhadis Yaru <mukyar@amazon.com>
2026-09-04 13:25:58 +0100

    Refactored the error handling and added comments

--- app/main.py
+++ app/main.py
@@ -7,33 +7,45 @@ import requests
 import time
 
 def get_latitude_and_longitude() -> tuple[int, int] | str:
+
+    # Error handling for the API request. If we cannot reach the geocoding API we handle it gracefully
     try:
         response = requests.get(coord_url, params=location_params)
-        data = response.json()
-        results = data["results"]
-        relevant_info = results[0]
-        latitude = relevant_info["latitude"]
-        longitude = relevant_info["longitude"]
-        coordinates = (latitude, longitude)
-        return coordinates
     except requests.exceptions.ConnectionError:
-        return "Error: Could not reach the geocoding API"
+        return f"Error: Could not reach the geocoding API"
+
+    # We manipulate the json data and extract the latitude and logitude we are looking for
+    data = response.json()
+    results = data["results"]
+    relevant_info = results[0]
+    latitude = relevant_info["latitude"]
+    longitude = relevant_info["longitude"]
+    coordinates = (latitude, longitude)
+    return coordinates
 
 def get_weather(coordinates: tuple[int, int] | str) -> tuple[int, int] | str:
+
+    # These are the parameters we are going to feed into the request as parameters
     weather_params = {
         "latitude": coordinates[0],
         "longitude": coordinates[1],
         "hourly": "temperature_2m"
     }
+
+    # Same logic as prior. If we cannot reach the Weather API. Handle the error gracefully
     try:
         response = requests.get(weather_url, params=weather_params)
+    except requests.exceptions.ConnectionError:
+        return f"Could not reach the weather API"
+
+    # We manipulate the data here to extract only the temperature for a particular hour in the day. If there is a KeyError we
+    # flag that as the previous API request failing and return the error we made for it
+    try:
         data = response.json()
         hourly_weather = data["hourly"]
         current_hour = time.localtime().tm_hour
         weather_that_hour = hourly_weather["temperature_2m"]
         return f"Weather is currently {weather_that_hour[current_hour]}º celsius in {city}."
-    except requests.exceptions.ConnectionError:
-        return f"Could not reach the weather API"
     except KeyError:
         return coordinates
 
