6866e3efbd5580bdcf82497f86fad6cb91f884d3
Mukhadis Yaru <mukyar@amazon.com>
2026-09-04 14:41:04 +0100

    chore: Added more precise error handling

--- app/constants.py
+++ app/constants.py
@@ -1,8 +1,10 @@
 city = "Dublin"
 
-coord_url = f"https://geocoding-api.open-meteo.com/v1/search"
-break_test_url = f"https://nonesense-url.ie"
-weather_url = f"https://api.open-meteo.com/v1/forecast"
+fake_url = "https://does-not-exist/v1/test.com"
+coord_url = "https://geocoding-api.open-meteo.com/v1/search"
+broken_coord_url = "https://geocoding-api.open-meteo.com/v1/does-not-exist"
+weather_url = "https://api.open-meteo.com/v1/forecast"
+broken_weather_url = "https://api.open-meteo.com/v1/does-not-exist"
 
 location_params = {
     "name": city,
--- app/main.py
+++ app/main.py
@@ -1,18 +1,26 @@
 from __future__ import annotations
 
-from .constants import city, coord_url, weather_url, location_params, break_test_url
+from requests.api import request
+
+from .constants import city, coord_url, fake_url, weather_url, location_params, broken_coord_url, broken_weather_url
 from typing import Any
 
 import requests
 import time
 
-def get_latitude_and_longitude() -> tuple[int, int] | str:
+def get_latitude_and_longitude(url: str) -> tuple[int, int] | str:
 
-    # Error handling for the API request. If we cannot reach the geocoding API we handle it gracefully
+    # Error handling for the API request. If the URL is wrong throw an error
     try:
-        response = requests.get(coord_url, params=location_params)
+        response = requests.get(url, params=location_params)
     except requests.exceptions.ConnectionError:
-        return f"Error: Could not reach the geocoding API"
+        return f"Could not reach '{url}'"
+
+    # Error handling for a server side issue. If it is a 4XX or 5XX
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        return f"ERROR: Could not reach the geocoding API [{response.status_code}]"
 
     # We manipulate the json data and extract the latitude and logitude we are looking for
     data = response.json()
@@ -23,7 +31,8 @@ def get_latitude_and_longitude() -> tuple[int, int] | str:
     coordinates = (latitude, longitude)
     return coordinates
 
-def get_weather(coordinates: tuple[int, int] | str) -> tuple[int, int] | str:
+
+def get_weather(coordinates: tuple[int, int] | str, url: str) -> tuple[int, int] | str:
 
     # These are the parameters we are going to feed into the request as parameters
     weather_params = {
@@ -32,24 +41,32 @@ def get_weather(coordinates: tuple[int, int] | str) -> tuple[int, int] | str:
         "hourly": "temperature_2m"
     }
 
-    # Same logic as prior. If we cannot reach the Weather API. Handle the error gracefully
+    # Same logic as prior. If the URL does not exist handle
     try:
-        response = requests.get(weather_url, params=weather_params)
+        response = requests.get(url, params=weather_params)
     except requests.exceptions.ConnectionError:
-        return f"Could not reach the weather API"
+        return f"Could not reach '{url}'"
 
-    # We manipulate the data here to extract only the temperature for a particular hour in the day. If there is a KeyError we
-    # flag that as the previous API request failing and return the error we made for it
+    # Error handling for a server side issue. If it is a 4XX or 5XX
     try:
+        # We manipulate the data here to extract only the temperature for a particular hour in the day. If there is a KeyError we
+        response.raise_for_status()
         data = response.json()
         hourly_weather = data["hourly"]
         current_hour = time.localtime().tm_hour
         weather_that_hour = hourly_weather["temperature_2m"]
         return f"Weather is currently {weather_that_hour[current_hour]}º celsius in {city}."
-    except KeyError:
-        return coordinates
+    except requests.exceptions.HTTPError:
+        if response.status_code == 400:
+            return coordinates
+        else:
+            return f"ERROR: Could not reach the weather API [{response.status_code}]"
+
+
 
 def main():
-    print(get_weather(get_latitude_and_longitude()))
+    coordinates = get_latitude_and_longitude(coord_url)
+    weather = get_weather(coordinates, weather_url)
+    print(weather)
 
 main()
